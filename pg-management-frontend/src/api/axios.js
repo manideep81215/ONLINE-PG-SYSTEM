5c95788c51623c8c58e3262fd2007651c57f7dce
manideep81215 <gannamanideep18@gmail.com>
2026-02-08 15:50:31 +0530

    Update axios.js

--- pg-management-frontend/src/api/axios.js
+++ pg-management-frontend/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
+export default axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+  withCredentials: true,
 });
-
-export default api;
