267a78467ee8dd0a61a3e6ebe848cf1b38bcb593
manideep81215 <gannamanideep18@gmail.com>
2026-02-08 16:00:58 +0530

    Update AuthController.java

--- management/src/main/java/com/pg/management/controller/AuthController.java
+++ management/src/main/java/com/pg/management/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+
 public class AuthController {
 
     private final WardenAuthService authService;
